4839a5a285f5b94c63950e078c7399a724f3a782
Arturo <grabowski.a93@gmail.com>
2019-06-09 18:07:18 +0200

    Function -> class

--- src/components/AvatarItem.js
+++ src/components/AvatarItem.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function AvatarItem({login}) {
-	let imgUrl = "dupa";
-	if (!login.includes("@")) {
-		imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
-	} else {
-		var gravatar = require('gravatar');
-		imgUrl = gravatar.url({login}, {s: '200', r: 'pg', d: '404'})
+
+class AvatarItem extends Component {
+	render() {
+		const {login} = this.props;
+		let imgUrl = "dupa";
+		if (!login.includes("@")) {
+			imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+		} else {
+			var gravatar = require('gravatar');
+			imgUrl = gravatar.url({login}, {s: '10', r: 'pg', d: '404'})
+		}
+
+
+		return (
+			<img src={imgUrl} className="ui mini rounded image" alt={"Ala ma kota"}/>
+		)
 	}
-
-
-	return (
-		<img src={imgUrl} className="ui mini rounded image" alt={"Ala ma kota"}/>
-	)
 }
 
 export default AvatarItem;
--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AvatarItem from './AvatarItem';
 
-function ContactItem({login, name, department }) {
-	return (
-		<li className="item">
-			<AvatarItem login = {login} />
-			<div className="content">
-				<h4 className="header">{name}</h4>
-				<div className="description">{department}</div>
-			</div>
-		</li>
-	);
+class ContactItem extends Component {
+	render() {
+		const {login, name, department } = this.props;
+		return (
+			<li className="item">
+				<AvatarItem login={login}/>
+				<div className="content">
+					<h4 className="header">{name}</h4>
+					<div className="description">{department}</div>
+				</div>
+			</li>
+		);
+	}
 }
 
 export default ContactItem;
