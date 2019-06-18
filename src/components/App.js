7ad93394b0c9574abbe6b871e82c9ed176ca7b08
Arturo <grabowski.a93@gmail.com>
2019-06-18 10:24:16 +0200

    Works but componentDidMount body is not clear

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 
-
 import AppHeader from './AppHeader';
 import ContactsList from './ContactsList';
 
+
 class App extends Component {
+	state = {
+		contacts: []
+	};
+
+	componentDidMount() {
+		fetch("https://randomuser.me/api/?format=json&results=10")
+			.then(res => res.json())
+			.then(json => this.setState({ contacts: json.results }));
+	}
+
+/*	componentDidMount() {
+		var data = require('../data/contacts.json'); // forward slashes will depend on the file location
+
+		for(var i = 0; i < data.length; i++) {
+			var obj = data[i];
+
+			console.log("Name: " + obj.first_name + ", " + obj.last_name);
+		}
+	}*/
+
+
 	render() {
+		const contacts = this.state.contacts;
 		return (
 			<div>
 				<AppHeader/>
@@ -15,7 +37,8 @@ class App extends Component {
 					<hr/>
 					<hr/>
 					<hr/>
-					<ContactsList/>
+					{/* eslint-disable-next-line no-undef */}
+					{contacts ? <ContactsList contacts={contacts} /> : 'Ładowanie…'}
 				</main>
 			</div>
 		);
